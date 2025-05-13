58e54cd50356a865a1e36e280ed7891323f56e73
minagishl <mvxd568.co@icloud.com>
2025-05-13 19:28:45 +0900

    build: update extension version

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 const manifest = defineManifest({
   manifest_version: 3,
   name: "Google Calendar Tonton",
-  version: "1.2.6",
+  version: "1.2.7",
   description: "Auto-send Tonton's schedule from Google Calendar.",
   permissions: ["storage"],
   host_permissions: ["https://calendar.google.com/*"],
